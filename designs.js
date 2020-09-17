7f487b9ec269c98d3ef0e99320c3b81afc1fef23
marta <martaespinosagarcia@gmail.com>
2020-09-17 19:32:55 +0200

    Initial commit

---
+++ designs.js
@@ -0,0 +1,37 @@
+// Select color input
+// Select size input
+var height, width, color;
+// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event) {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWidth').val();
+    makeGrid(height, width);
+
+})
+
+
+
+function makeGrid(x, y) {
+    $('tr').remove();
+
+    // Your code goes here!
+    for (var i = 1; i <= x; i++) {
+        $('#pixelCanvas').append('<tr id = table' + i + '></tr>');
+        for (var z = 1; z <= y; z++) {
+            $('#table' + i).append('<td></td>');
+        }
+    }
+
+    $('td').click(function addColor() {
+        color = $('#colorPicker').val();
+
+        if ($(this).attr('style')) {
+            $(this).removeAttr('style')
+        } else {
+            $(this).attr('style', 'background-color:' + color);
+        }
+
+    })
+
+}
